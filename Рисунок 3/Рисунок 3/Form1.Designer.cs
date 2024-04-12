cb25e3f98edd32f444c7af3080c8a7330967793a
GHOST <luchshevdanil005@gmail.com>
2024-04-12 09:37:23 +0300

    рисунок 3

---
+++ Рисунок 3/Рисунок 3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Рисунок_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButtonSqrt = new System.Windows.Forms.RadioButton();
+            this.radioButtonHyperbola = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(668, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(686, 69);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(701, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Кнопка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButtonSqrt
+            // 
+            this.radioButtonSqrt.AutoSize = true;
+            this.radioButtonSqrt.Location = new System.Drawing.Point(686, 116);
+            this.radioButtonSqrt.Name = "radioButtonSqrt";
+            this.radioButtonSqrt.Size = new System.Drawing.Size(75, 20);
+            this.radioButtonSqrt.TabIndex = 5;
+            this.radioButtonSqrt.TabStop = true;
+            this.radioButtonSqrt.Text = "Корень";
+            this.radioButtonSqrt.UseVisualStyleBackColor = true;
+            this.radioButtonSqrt.CheckedChanged += new System.EventHandler(this.radioButtonSqrt_CheckedChanged);
+            // 
+            // radioButtonHyperbola
+            // 
+            this.radioButtonHyperbola.AutoSize = true;
+            this.radioButtonHyperbola.Location = new System.Drawing.Point(685, 152);
+            this.radioButtonHyperbola.Name = "radioButtonHyperbola";
+            this.radioButtonHyperbola.Size = new System.Drawing.Size(99, 20);
+            this.radioButtonHyperbola.TabIndex = 6;
+            this.radioButtonHyperbola.TabStop = true;
+            this.radioButtonHyperbola.Text = "Гипербола";
+            this.radioButtonHyperbola.UseVisualStyleBackColor = true;
+            this.radioButtonHyperbola.CheckedChanged += new System.EventHandler(this.radioButtonHyperbola_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonHyperbola);
+            this.Controls.Add(this.radioButtonSqrt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButtonSqrt;
+        private System.Windows.Forms.RadioButton radioButtonHyperbola;
+    }
+}
+
---
+++ Рисунок 3/Рисунок 3/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Рисунок_3
+{
+    public partial class Form1 : Form
+    {
+        int kx = 50;
+        int ky = 50;
+        int dy;
+        int dx;
+        Bitmap graphBitmap;
+        Graphics graphGraphics;
+        public Form1()
+        {
+
+            InitializeComponent();
+            dy = pictureBox1.Height / 2;
+            dx = pictureBox1.Width / 2;
+            pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (radioButtonHyperbola.Checked)
+            {
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            graphGraphics = e.Graphics;
+            SolidBrush solidBrush = new SolidBrush(Color.White);
+            graphGraphics.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Pen p3 = new Pen(Color.Black);
+            p3.Width = 2;
+            graphGraphics.DrawLine(p3, 0, dy, pictureBox1.Width, dy);
+            graphGraphics.DrawLine(p3, dx, 0, dx, pictureBox1.Height);
+
+            if (radioButtonSqrt.Checked)
+            {
+                DrawSqrtFunction();
+            }
+            else if (radioButtonHyperbola.Checked)
+            {
+                DrawHyperbola();
+            }
+        }
+
+        private void DrawHyperbola()
+        {
+            double a = Convert.ToDouble(numericUpDown1.Value);
+            Pen pen = new Pen(Color.Red);
+            pen.Width = 3;
+            Point[] points = new Point[0];
+            int i = 0;
+            for (double x = 0.1; x <= 10; x += 0.1)
+            {
+                Array.Resize(ref points, i + 1);
+                double y = a / x;
+                points[i] = new Point(Convert.ToInt32(x * kx) + dx, Convert.ToInt32(-y * ky) + dy);
+                i++;
+            }
+            graphGraphics.DrawLines(pen, points);
+        }
+
+        private void DrawSqrtFunction()
+        {
+            Pen pen = new Pen(Color.Blue);
+            pen.Width = 3;
+            Point[] points = new Point[0];
+            int i = 0;
+            for (double x = 0.1; x <= 10; x += 0.1) 
+            {
+                Array.Resize(ref points, i + 1);
+                double y = x * x; 
+                points[i] = new Point(Convert.ToInt32(x * kx) + dx, Convert.ToInt32(-y * ky) + dy);
+                i++;
+            }
+            graphGraphics.DrawLines(pen, points);
+        }
+
+    
+
+        private void radioButtonHyperbola_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonSqrt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonSqrt.Checked)
+            {
+                pictureBox1.Refresh();
+            }
+        }
+    }
+}
